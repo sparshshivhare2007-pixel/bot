6309c4b10c9c4580b50aebd29786a465993d85dd
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-11-28 11:42:52 +0530

    Update hug.py

--- commands/hug.py
+++ commands/hug.py
@@ -1,20 +1,58 @@
-import os, random
 from telegram import Update
-from telegram.ext import ContextTypes
+from telegram.ext import ContextTypes, CommandHandler
+import random
 
-ASSETS_PATH = "assets"
+# 10 Hug GIFs
+HUG_GIFS = [
+    "https://media.giphy.com/media/l2QDM9Jnim1YVILXa/giphy.gif",
+    "https://media.giphy.com/media/od5H3PmEG5EVq/giphy.gif",
+    "https://media.giphy.com/media/xT0GqeSlGSRQutnC2g/giphy.gif",
+    "https://media.giphy.com/media/wnsgren9NtITS/giphy.gif",
+    "https://media.giphy.com/media/3ZnBrkqoaI2hq/giphy.gif",
+    "https://media.giphy.com/media/49mdjsMrH7oze/giphy.gif",
+    "https://media.giphy.com/media/svXXBgduBsJ1u/giphy.gif",
+    "https://media.giphy.com/media/PHZ7v9tfQu0o0/giphy.gif",
+    "https://media.giphy.com/media/BXrwTdoho6hkQ/giphy.gif",
+    "https://media.giphy.com/media/xT8qBepJQG8OZX9Ji0/giphy.gif"
+]
 
 async def hug(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if not context.args:
-        await update.message.reply_text("❌ Usage: /hug @username")
+
+    # Hugger (user sending command)
+    hugger = update.effective_user
+    hugger_name = hugger.first_name
+    hugger_id = hugger.id
+
+    # Target user (reply required)
+    if update.message.reply_to_message:
+        target = update.message.reply_to_message.from_user
+    elif context.args:
+        await update.message.reply_text("Reply karke hug karo taaki ID mil sake! 🤗")
+        return
+    else:
+        await update.message.reply_text("Kisko hug karna hai? Reply karo! 🫂")
         return
 
-    target = " ".join(context.args)
-    message = f"🤗 {update.effective_user.first_name} sent a hug to {target}!"
-    await update.message.reply_text(message)
+    target_name = target.first_name
+    target_id = target.id
 
-    # Random video from hug folder
-    hug_videos = os.listdir(os.path.join(ASSETS_PATH, "gifs", "hug"))
-    video_file = os.path.join(ASSETS_PATH, "gifs", "hug", random.choice(hug_videos))
+    # Random GIF
+    gif_url = random.choice(HUG_GIFS)
 
-    await update.message.reply_video(video=open(video_file, "rb"))
+    # Caption with clickable IDs
+    caption = (
+        f"<b><a href='tg://user?id={hugger_id}'>{hugger_name}</a></b> "
+        f"hugs "
+        f"<b><a href='tg://user?id={target_id}'>{target_name}</a></b> 🤗"
+    )
+
+    # Send GIF
+    await update.message.reply_animation(
+        animation=gif_url,
+        caption=caption,
+        parse_mode="HTML"
+    )
+
+# --------------------------
+# Add this handler in main.py
+# application.add_handler(CommandHandler("hug", hug))
