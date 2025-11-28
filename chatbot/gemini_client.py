d205028e3486d5facfbb4a673c1c51aa07cfbb47
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-11-28 11:10:07 +0530

    Update gemini_client.py

--- chatbot/gemini_client.py
+++ chatbot/gemini_client.py
@@ -1,16 +1,16 @@
 import os
-import google.generativeai as genai
 from dotenv import load_dotenv
+import google.generativeai as genai
 
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=os.getenv("GEMINI_API"))
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+client = genai.GenerativeModel("gemini-1.5-flash")
 
 async def ask_gemini(text):
     try:
-        response = model.generate_content(text)
+        response = client.generate_content(contents=text)
         return response.text
     except Exception as e:
         return f"⚠️ Error: {e}"
