e977598d097e5d1cda58496fe44859a8e4e86d33
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-11-28 23:46:08 +0530

    Create bank.py

---
+++ commands/bank.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+from helpers import get_user
+
+async def bank(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = get_user(update.effective_user.id)
+    bank_amount = user.get("bank", 0)
+
+    await update.message.reply_text(
+        f"🏦 *Bank Account*\n"
+        f"💳 Balance in bank: **${bank_amount}**",
+        parse_mode="Markdown"
+    )
