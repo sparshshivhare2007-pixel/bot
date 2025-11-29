2d569a13e4bd26c7540255f4a944c946e5291152
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-11-29 22:16:40 +0530

    Update chats.py

--- chatbot/database/chats.py
+++ chatbot/database/chats.py
@@ -1,16 +1,12 @@
-from pymongo import MongoClient
-from chatbot.config import MONGO_URI
+from . import chatsdb
 
-db = MongoClient(MONGO_URI)["chatbot_db"]
-chatsdb = db["chats"]
-
-async def get_served_chats():
-    return [c async for c in chatsdb.find()]
-
-async def is_served_chat(chat_id: int):
-    x = await chatsdb.find_one({"chat_id": chat_id})
-    return bool(x)
+async def is_served_chat(chat_id: int) -> bool:
+    chat = await chatsdb.find_one({"chat_id": chat_id})
+    return bool(chat)
 
 async def add_chat(chat_id: int):
     if not await is_served_chat(chat_id):
         await chatsdb.insert_one({"chat_id": chat_id})
+
+async def get_served_chats():
+    return [c async for c in chatsdb.find()]
