26140bfcd04f2de8c344e33d998bdcf5b553c6d4
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-11-29 23:00:45 +0530

    Update start.py

--- chatbot/modules/start.py
+++ chatbot/modules/start.py
@@ -1,22 +1,7 @@
-import asyncio
-import random
-from pyrogram import filters
-from pyrogram.enums import ChatType
-from pyrogram.types import InlineKeyboardMarkup
-from chatbot import ChatBot
-from chatbot.database.chats import add_served_chat
-from chatbot.database.users import add_served_user
-from config import IMG, STICKER, DEV_OP, START, HELP_BTN
+from telegram import Update
+from telegram.ext import ContextTypes
 
-@ChatBot.on_message(filters.command(["start", "aistart"]))
-async def start(_, message):
-    if message.chat.type == ChatType.PRIVATE:
-        accha = await message.reply_text(random.choice(["Starting..."]))
-        await asyncio.sleep(1)
-        await accha.delete()
-        await message.reply_sticker(sticker=random.choice(STICKER))
-        await message.reply_photo(photo=random.choice(IMG), caption=f"Hi, I am ChatBot", reply_markup=InlineKeyboardMarkup(DEV_OP))
-        await add_served_user(message.from_user.id)
-    else:
-        await message.reply_photo(photo=random.choice(IMG), caption=START, reply_markup=InlineKeyboardMarkup(DEV_OP))
-        await add_served_chat(message.chat.id)
+async def chatbot_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Hello! I'm your friendly chatbot 😊\nHow can I help you today?"
+    )
