95c07aa6e250736328ebdb9a26d96952595ba03e
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-11-29 22:52:44 +0530

    Update chatbot.py

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,10 +1,16 @@
 from pyrogram import Client
-from .config import API_ID, API_HASH, BOT_TOKEN_CHAT
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 ChatBot = Client(
     "ChatBot",
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN_CHAT,
-    plugins=dict(root="chatbot/modules")
+    bot_token=BOT_TOKEN
 )
