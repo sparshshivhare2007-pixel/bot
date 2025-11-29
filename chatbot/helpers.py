9176abfa196edff72cb1e8d56c096607adb3e4fd
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-11-29 21:39:12 +0530

    Update helpers.py

--- chatbot/helpers.py
+++ chatbot/helpers.py
@@ -1,3 +1,4 @@
+# chat/helpers.py
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
@@ -6,31 +7,20 @@ load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URL")
 client = MongoClient(MONGO_URL)
-chat_db = client["chatbot_db"]
+chat_db = client["chat_bot"]
 
-# Collections
-users = chat_db["users"]
-sessions = chat_db["sessions"]  # optional, if chatbot uses session tracking
+# Messages / users collection
+chat_users = chat_db["users"]
 
-# ------------------- CHAT USER SYSTEM -------------------
 def get_chat_user(user_id):
-    user = users.find_one({"user_id": user_id})
+    user = chat_users.find_one({"user_id": user_id})
     if not user:
-        users.insert_one({
-            "user_id": user_id,
-            "messages": [],
-            "joined": True
-        })
-        user = users.find_one({"user_id": user_id})
+        chat_users.insert_one({"user_id": user_id, "messages": []})
+        user = chat_users.find_one({"user_id": user_id})
     return user
 
 def add_message(user_id, message):
-    users.update_one(
-        {"user_id": user_id},
-        {"$push": {"messages": message}},
-        upsert=True
-    )
+    chat_users.update_one({"user_id": user_id}, {"$push": {"messages": message}}, upsert=True)
 
-# ------------------- UTILS -------------------
 def is_user_exists(user_id):
-    return users.find_one({"user_id": user_id}) is not None
+    return chat_users.find_one({"user_id": user_id}) is not None
