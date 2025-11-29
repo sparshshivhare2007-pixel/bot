8547622ad172a4898e16d1200692ba21fca2fa34
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-11-29 23:07:04 +0530

    Update ids.py

--- chatbot/modules/ids.py
+++ chatbot/modules/ids.py
@@ -1,15 +1,10 @@
-from pyrogram import filters
-from pyrogram.enums import ParseMode
-from chatbot import ChatBot
+# Simple command to show user ID / chat ID
+from telegram import Update
+from telegram.ext import ContextTypes
 
-@ChatBot.on_message(filters.command("id"))
-async def get_id(client, message):
-    reply = message.reply_to_message
-    text = f"✨ <b>ID Information</b>\n\n"
-    text += f"📩 Message ID: {message.id}\n"
-    text += f"🙋 Your ID: {message.from_user.id}\n"
-    text += f"💬 Chat ID: {message.chat.id}\n\n"
-    if reply and reply.from_user:
-        text += f"🔁 Replied Msg ID: {reply.id}\n"
-        text += f"👤 Replied User ID: {reply.from_user.id}\n"
-    await message.reply_text(text, disable_web_page_preview=True, parse_mode=ParseMode.HTML)
+async def my_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat = update.effective_chat
+    user = update.effective_user
+    await update.message.reply_text(
+        f"User ID: {user.id}\nChat ID: {chat.id}"
+    )
