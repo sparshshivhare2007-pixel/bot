8a3c430d1ba8fc28445b3e2be3040718813b12da
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-11-29 22:16:25 +0530

    Update users.py

--- chatbot/database/users.py
+++ chatbot/database/users.py
@@ -1,16 +1,12 @@
-from pymongo import MongoClient
-from chatbot.config import MONGO_URI
+from . import usersdb
 
-db = MongoClient(MONGO_URI)["chatbot_db"]
-users = db["users"]
-
-async def get_served_users():
-    return [u async for u in users.find()]
-
-async def is_user_exists(user_id: int):
-    x = await users.find_one({"user_id": user_id})
-    return bool(x)
+async def is_user_exists(user_id: int) -> bool:
+    user = await usersdb.find_one({"user_id": user_id})
+    return bool(user)
 
 async def add_user(user_id: int):
     if not await is_user_exists(user_id):
-        await users.insert_one({"user_id": user_id})
+        await usersdb.insert_one({"user_id": user_id})
+
+async def get_served_users():
+    return [u async for u in usersdb.find()]
