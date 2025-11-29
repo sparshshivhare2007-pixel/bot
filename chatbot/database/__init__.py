62b33779e65402b582d0e4e31ac92a50916d326d
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-11-29 22:54:13 +0530

    Update __init__.py

--- chatbot/database/__init__.py
+++ chatbot/database/__init__.py
@@ -1,8 +1,1 @@
-from pymongo import MongoClient
-import config  # Make sure you have config.py with MONGO_URL
-
-mongo = MongoClient(config.MONGO_URL)
-db = mongo.ChatDb  # Chatbot-specific database
-
-chatsdb = db["chatsdb"]
-usersdb = db["users"]
+# empty
