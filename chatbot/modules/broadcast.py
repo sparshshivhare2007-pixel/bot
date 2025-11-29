7d11c5c1e0dde05c6af127ce1a55e975cc45d1b4
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-11-29 22:56:06 +0530

    Update broadcast.py

--- chatbot/modules/broadcast.py
+++ chatbot/modules/broadcast.py
@@ -1,24 +1,17 @@
-import asyncio
 from pyrogram import filters
-from pyrogram.errors import FloodWait
-from pyrogram.types import Message
-from chatbot import ChatBot
-from chatbot.database.chats import get_served_chats
-from chatbot.database.users import get_served_users
+from chatbot.chatbot import ChatBot
 from config import OWNER_ID
 
-@ChatBot.on_message(filters.command("broadcast") & filters.user(OWNER_ID))
-async def broadcast(_, message: Message):
-    reply = message.reply_to_message
+@ChatBot.on_message(filters.command("cbroadcast") & filters.user(OWNER_ID))
+async def broadcast(bot, message):
     text = message.text.split(None, 1)[1] if len(message.command) > 1 else None
+    if not text:
+        return await message.reply("Broadcast text do.")
 
-    if not reply and not text:
-        return await message.reply_text("❖ Reply to a message or provide text to broadcast.")
+    async for dialog in bot.get_dialogs():
+        try:
+            await bot.send_message(dialog.chat.id, text)
+        except:
+            pass
 
-    progress_msg = await message.reply_text("❖ Broadcasting message... Please wait")
-    sent_groups, sent_users, failed, pinned = 0, 0, 0, 0
-
-    chats_data = await get_served_chats()
-    users_data = await get_served_users()
-
-    recipients = [cha]()
+    await message.reply("Broadcast done!")
