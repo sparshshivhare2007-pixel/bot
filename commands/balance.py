6d9e5c49060abc1260a1361a5a3b3faebc2037f9
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-12-04 16:43:50 +0530

    Delete commands directory

--- commands/balance.py
+++
@@ -1,5 +0,0 @@
-@app.on_message(filters.command("balance") & filters.private)
-async def balance(client: Client, message: Message):
-    user = get_user(message.from_user.id)
-    balance = user.get("balance", 0)
-    await message.reply_text(f"💰 Your balance: ${balance}")
