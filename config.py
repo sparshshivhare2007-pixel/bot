e736f73091cf89cb93493df3eba8b596ab330bb3
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-12-04 16:53:24 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-API_ID = int(os.getenv("API_ID", "YOUR_API_ID"))
-API_HASH = os.getenv("API_HASH", "YOUR_API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID", "YOUR_ID"))
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
